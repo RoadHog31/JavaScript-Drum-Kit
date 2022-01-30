54cda7b64c79b080050be2d1f21d9d521acaa024
Your Name <you@example.com>
2022-01-30 17:46:24 +0000

    Removetransition function added aswell as transform CSS.

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-
-//e is a KeyboardEvent object full of data such as key code
-window.addEventListener("keydown", function (e) {
+function playSound(e) {
     
     //console.log(e.key);
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
@@ -9,6 +7,23 @@ window.addEventListener("keydown", function (e) {
     if (!audio) return; //stop the function from running all together. 
     audio.currentTime = 0; //rewind to start
     audio.play();
-    console.log(key);
+    //console.log(key);
+    key.classList.add("playing");
+};
 
-});
+function removeTransition(e) {
+    
+    //console.log(e);
+    if (e.propertyName !== "transform") return; //skip if not a transform
+    //console.log(e.propertyName);
+    this.classList.remove("playing"); 
+
+};
+
+//When have array of elements you have to loop over them. 
+const keys = document.querySelectorAll(".key");
+//listen for transition end event. 
+keys.forEach(key => key.addEventListener("transitionend", removeTransition));
+
+//e is a KeyboardEvent object full of data such as key code
+window.addEventListener("keydown", playSound);
